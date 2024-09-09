c6c0537b7d0a69d3f37aa3edd07b20e1a1d21ed9
Panodra <pandora6722@gmail.com>
2024-09-10 06:03:37 +0900

    주석 기능

--- MyCProject/hello.c
+++ MyCProject/hello.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
+	/*
+	주석 공간에서는 어떠한 텍스트를 써도 작동에 영향을 주지 않는다.
+	가나다라마바사아자차카타파하 ABCDEFGHIJKLMNOPQRSTUVWXYZ
+	printf("Hello, Computre"); << 주석 안에서의 코드라 작동하지 않는다.
+	*/
+
+	// Hello, World! 를 출력한다.
 	printf("Hello, World! \n");
 	return 0;
 }
